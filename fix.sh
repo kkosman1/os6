dcc1f75ccc305977fb6dcda56508323852692ed0
Sarah Elizabeth Olson <solson3@student02.cse.nd.edu>
2017-05-02 18:48:43 -0400

    Added fix commit

---
+++ fix.sh
@@ -0,0 +1,4 @@
+rm image.*
+wget http://www3.nd.edu/~dthain/courses/cse30341/spring2017/project6/image.5
+wget http://www3.nd.edu/~dthain/courses/cse30341/spring2017/project6/image.20
+wget http://www3.nd.edu/~dthain/courses/cse30341/spring2017/project6/image.200
